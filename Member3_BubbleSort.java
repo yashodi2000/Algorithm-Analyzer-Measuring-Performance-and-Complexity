7185feae38764539dd49b0b3bf90d798ace269c9
Ranul Hansaja <ranulh6@gmail.com>
2025-11-10 23:37:04 +0530

    setup main class and display program header

--- Member3_BubbleSort.java
+++ Member3_BubbleSort.java
@@ -1,1 +1,7 @@
-
+public class Member3_BubbleSort {
+    public static void main(String[] args) {
+        System.out.println("Algorithm: Bubble Sort");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
+    }
+}
