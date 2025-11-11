49636a11eb50b1abe87440edb8af26f443ae2dd2
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-11-11 11:12:19 +0530

    5.Measure Time and Print Results
    Measure execution time of Quick Sort and print results for different input sizes.

--- Member4_QuickSort.java
+++ Member4_QuickSort.java
@@ -41,3 +41,13 @@ public class Member4_QuickSort {
         arr[high] = temp;
         return i + 1;
     }
+
+    public static int[] generateRandomArray(int size) {
+        Random rand = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
+        return arr;
+    }
+}
