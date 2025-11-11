caa112ec274fa9e259164bf3b884e2190f8a62e3
Sithija194 <sithijant231@gmail.com>
2025-11-11 14:08:12 +0530

    This loop tests the linear search with arrays of different sizes and prints the time taken for each.

--- LinearSearch.java
+++ LinearSearch.java
@@ -5,3 +5,15 @@ public class Member1_LinearSearch {
         System.out.println("Algorithm: Linear Search");
         System.out.println("Input Size | Time (ms)");
         System.out.println("----------------------");
+
+         int[] sizes = {100, 500, 1000};
+        for (int size : sizes) {
+            int[] arr = generateRandomArray(size);
+            int target = arr[new Random().nextInt(size)]; // Random target element
+            long startTime = System.nanoTime();
+            linearSearch(arr, target);
+            long endTime = System.nanoTime();
+            double timeTaken = (endTime - startTime) / 1_000_000.0;
+            System.out.printf("%-10d | %.4f%n", size, timeTaken);
+        }
+    }
