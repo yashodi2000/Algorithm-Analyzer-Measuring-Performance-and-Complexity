bcb5d5dc5880c832789fc1c5e3f6c95a0c54d58c
Sithija194 <sithijant231@gmail.com>
2025-11-11 14:06:25 +0530

    This method displays the algorithm name and prepares a table header for showing results.

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,1 +1,7 @@
+import java.util.Random;
 
+public class Member1_LinearSearch {
+    public static void main(String[] args) {
+        System.out.println("Algorithm: Linear Search");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
