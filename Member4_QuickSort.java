7fb3fb1deda988f81c26523bec64ef18ca1441c0
Y.C.ABEYSUNDARA <22ug3-0016@sltc.ac.lk>
2025-11-11 11:00:56 +0530

    2.Add Main Method and Print Statements.
    Add main method and print headers for Quick Sort measurement.

--- Member4_QuickSort.java
+++ Member4_QuickSort.java
@@ -1,3 +1,10 @@
 import java.util.Random;
 
 public class Member4_QuickSort {
+     public static void main(String[] args) {
+        System.out.println("Algorithm: Quick Sort");
+        System.out.println("Input Size | Time (ms)");
+        System.out.println("----------------------");
+
+        int[] sizes = {100, 500, 1000};
+        for (int size : sizes) {
